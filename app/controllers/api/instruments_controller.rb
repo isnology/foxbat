1d07811f3f42241c53b90ca78bfb364e1822212a
IsNology <glenn@isnology.com>
2018-05-12 01:27:31 +1000

    cleanup after change to ActiveModel::Serializer

--- app/controllers/api/instruments_controller.rb
+++ app/controllers/api/instruments_controller.rb
@@ -2,18 +2,12 @@ class Api::InstrumentsController < ApplicationController
   before_action :authenticate_request!, only: [:create, :update]
   
   def index
-    #render json: format_many(Instrument.includes(:instrument_class).all),
-    #       status: :ok
-    #render json: Api::InstrumentSerializer.new(Instrument.includes(:instrument_class).all).serialized_json,
-    #       status: :ok
-    instruments = Instrument.includes(:instrument_class).all
-    render json: instruments, status: :ok
+    render json: Instrument.includes(:instrument_class).all, status: :ok
   end
 
   def create
     instrument = Instrument.includes(:instrument_class).new(instrument_params)
     if instrument.save
-      #render json: format(instrument), status: :created
       render json: instrument, status: :created
     else
       render json: instrument.errors, status: :unprocessable_entity
@@ -23,7 +17,6 @@ class Api::InstrumentsController < ApplicationController
   def update
     instrument = Instrument.includes(:instrument_class).find(params[:id])
     if instrument.update(instrument_params)
-      #render json: format(instrument), status: :ok
       render json: instrument, status: :ok
     else
       render json: instrument.errors, status: :unprocessable_entity
